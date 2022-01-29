c5d8c1b79dcd225daf382d6d92524ed6b8bcb7f6
Samantha Diaz <samidiaz3@gmail.com>
2022-01-29 00:07:39 -0500

    commit tree 8eba02c9096e42d32808e319d08102706781e415

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+// SOLUTION 
+// let wrapAdjective = function(style="*") {
+//     return function(adjective="special") {
+//       return `You are ${style}${adjective}${style}!`
+//     }
+//   }
+
+const Calculator = {
+    add: function(a,b) {
+    return a + b
+    }, 
+
+    subtract: function(a,b) {
+        return a - b
+    }, 
+
+    multiply: function(a,b) {
+        return a * b
+    }, 
+
+    divide: function(a,b) {
+        return a / b
+    }
+}
+
+// copied this one!
+let actionApplyer = function(startInt, functionArray){
+
+    for (let i = 0; i < functionArray.length; i++){
+        startInt = functionArray[i](startInt)
+    }
+
+    return startInt
+}
+
+
